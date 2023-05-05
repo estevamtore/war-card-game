cae1e714d782761cfb06bb9eb1038e395ababae4
Estevam <estevamtorezani@hotmail.com>
2023-05-05 00:11:36 -0300

    Initial Commit

---
+++ war card game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+//
+//  Created by Estevam Torezani on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
